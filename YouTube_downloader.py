d4c6755ba9f42cf457f3e151bf8c1bfd2aa3cc1d
smsh51 <52484844+smsh51@users.noreply.github.com>
2023-04-19 17:32:35 +0200

    Update YouTube_downloader.py

--- YouTube_downloader.py
+++ YouTube_downloader.py
@@ -9,20 +9,21 @@ from pytube import YouTube
 link = input("Enter the link of YouTube video you want to download:  ")
 if link == 'test':
   link = 'https://www.youtube.com/watch?v=TX4Awy03dH0'
-yt = YouTube(link)
+  
+  yt = YouTube(link)
 
-# Showing details
-print("Title: ",yt.title)
-print("Number of views: ",yt.views)
-print("Length of video: ",yt.length)
-print("Rating of video: ",yt.rating)
+  # Showing details
+  print("Title: ",yt.title)
+  print("Number of views: ",yt.views)
+  print("Length of video: ",yt.length)
+  print("Rating of video: ",yt.rating)
 
-ys = yt.streams.first()
+  ys = yt.streams.first()
 
-# Getting the highest resolution possible
-ys = yt.streams.get_highest_resolution()
+  # Getting the highest resolution possible
+  ys = yt.streams.get_highest_resolution()
 
-# Starting download
-print("Downloading...")
-ys.download()
-print("Download completed!!")
+  # Starting download
+  print("Downloading...")
+  ys.download()
+  print("Download completed!!")
